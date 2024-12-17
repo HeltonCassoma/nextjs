34f78475c6cd6fcff940e210dbea8f6de0b9f5a3
HeltonCassoma <96545389+HeltonCassoma@users.noreply.github.com>
2024-12-17 18:03:31 +0000

    ainda tem erro

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function page(){
   return <>
-  <h1> React e Next.js</h1>
-  <p>Bem-vindo à minha app em React e Next.js,
-  das tecnologias Web mais usadas nos dias de hoje.</p>
+  <h1> React e Next.js </h1>
+  <p> Bem-vindo à minha app em React e Next.js,
+  das tecnologias Web mais usadas nos dias de hoje. </p>
   </>
 }
