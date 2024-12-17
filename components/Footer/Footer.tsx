3bdea06d12c2d8f68ebf26d372179097a551be2d
HeltonCassoma <96545389+HeltonCassoma@users.noreply.github.com>
2024-12-17 16:17:03 +0000

    fiz o footer e o header

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Footer.module.css'
+
+{/* É assim que se faz um comentário!! */}
+
+export default function Footer(){
+    return <footer className={styles.footer}>
+    2024.
+    </footer>
+}
+
+
+{/* Exemplo de utilização do CSS com Tailwid*/}
+{/* <footer className="bg=none color:white font-weight:bold padding: 10px 
+   text-align:center"> 2024</footer>*/}
